92c811e4d28a1084576d987464fc95f21d0cd3e7
learntopilk <jonsterivilja@gmail.com>
2018-02-21 20:48:53 +0200

    testiDao luotu, main.javassa sille testireitti. Näitä ei kenkään tule käyttämän muuta kuin testitarkoituksiin. Lopullisesta versiosta ne poistetaan, sillä ne ovat vain PostGreSQL:n testaamiseksi tarkoitettu.

--- src/main/java/Webserver/Main.java
+++ src/main/java/Webserver/Main.java
@@ -2,26 +2,36 @@ package Webserver;
 
 import java.util.*;
 import spark.*;
-//import java.sql.*;
+import Webserver.Database;
+import java.sql.*;
 //import java.io.File;
-//import java.util.ArrayList;
-//import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
 //import java.util.List;
 //import java.util.List;
-//import spark.ModelAndView;
-//import spark.Spark;
-//import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import spark.ModelAndView;
 import spark.TemplateEngine;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
-//import tikape.Database;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // Luodaan TestiDao herokun tietokannan testausta varten
+        TestiDao testi = new TestiDao();
+
+        for (int i = 0; i < 10; i++) {
+
+            testi.saveOne((int) (Math.random() * 1000));
+
+        }
+
+        // Tämä asettaa herokun portin ympäristömuuttujan määräämäksi,
+        // jos ympäristömuuttuja on olemassa. Herokua varten tärkeä!
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
+
         System.out.println("Server started.");
 
         // TODO: Database-yhteyden luominen
@@ -31,6 +41,12 @@ public class Main {
         raakaaineet.add(new Raaka_aine("suoli"));
         raakaaineet.add(new Raaka_aine("suole"));
 
+        /**
+         *
+         * R E I T I T
+         *
+         *
+         */
         Spark.get("*", (req, res) -> {
 
             HashMap map = new HashMap<>();
@@ -39,6 +55,30 @@ public class Main {
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
+
+        Spark.get("/reseptit", (req, res) -> {
+
+            HashMap map = new HashMap<>();
+
+            // TODO: 
+            map.put("raaka_aineet", raakaaineet);
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+
+        Spark.get("/testi", (req, res) -> {
+
+            //HashMap map = new HashMap<>();
+            System.out.println("Tallennusta tehdään testimielessä!");
+
+            testi.saveOne((int) (Math.random() * 1000));
+            //map.put("raaka_aineet", raakaaineet);
+
+            res.redirect("/");
+            return " ";
+
+            // return new ModelAndView(map, "index");
+        });
 
         System.out.println("");
     }
