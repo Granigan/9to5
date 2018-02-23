08aad219cf5a982fe56f6d5f3629db1a2a522ad4
Tapio Tero M <tgtapio@lx7-fuxi311.ad.helsinki.fi>
2018-02-23 21:34:50 +0200

    RaakaaineDao.javassa nyt toimiva findOne ja saveOrUpdate, mainissa testirivit em.

--- src/main/java/DAOs/RaakaaineDao.java
+++ src/main/java/DAOs/RaakaaineDao.java
@@ -7,33 +7,49 @@ import java.util.List;
 
 /*
 DAO raaka-aineille, constructorissa create table
-*/
-
+ */
 public class RaakaaineDao implements Dao {
 
     public RaakaaineDao() throws SQLException {
         try {
             Connection con = getConnection();
             PreparedStatement createTable = con.prepareStatement(
-                "CREATE TABLE IF NOT EXISTS Raakaaine ("
-                        + "id SERIAL PRIMARY KEY,"
-                        + "nimi varchar(50),"
-                        + "mittayksikko varchar(50),"
-                        + "kuvaus varchar(500)"
-                        + ");");
+                    "CREATE TABLE IF NOT EXISTS Raakaaine ("
+                    + "id SERIAL PRIMARY KEY,"
+                    + "nimi varchar(50),"
+                    + "mittayksikko varchar(50),"
+                    + "kuvaus varchar(500)"
+                    + ");");
             createTable.executeUpdate();
             createTable.close();
             con.close();
-            
+
         } catch (Exception e) {
             System.out.println("ongelma luodessa raakaaine-taulua: " + e.getMessage());
         }
-        
+
     }
 
     @Override
     public Object findOne(Object key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Raaka_aine etsittava = (Raaka_aine) key;
+            Raaka_aine osuma = null;
+            Connection con = getConnection();
+            PreparedStatement haku = con.prepareStatement(""
+                    + "SELECT * FROM Raakaaine WHERE nimi = ?");
+            haku.setString(1, etsittava.getNimi());
+            ResultSet rs = haku.executeQuery();
+            if (rs.next()) {
+                osuma = new Raaka_aine(rs.getInt("id"), rs.getString("nimi"), rs.getString("mittayksikko"), rs.getString("kuvaus"));
+            }
+            
+            return osuma;
+
+        } catch (SQLException e) {
+            System.out.println("ongelma etsiessä yhtä raaka-ainetta" + e.getMessage());
+        }
+        return null;
     }
 
     @Override
@@ -43,35 +59,44 @@ public class RaakaaineDao implements Dao {
 
     @Override
     public Object saveOrUpdate(Object raakaaine) throws SQLException {
-        try{
-        Raaka_aine uusi = (Raaka_aine) raakaaine;
-        // tästä puuttuu tarkistus onko raaka-ainetta jo olemassa
-        
-        Connection con = getConnection();
-        PreparedStatement lisaa = con.prepareStatement("INSERT INTO Raakaine (nimi, mittayksikko, kuvaus) "
-                + "VALUES ? ? ?");
-        lisaa.setString(1, uusi.getNimi());
-        lisaa.setString(2, uusi.getMittayksikko());
-        lisaa.setString(3, uusi.getKuvaus());
-        
-        lisaa.executeUpdate();
-        lisaa.close();
-        con.close();
-        
-        
+        try {
+            Raaka_aine uusi = (Raaka_aine) raakaaine;
+            if (findOne(uusi) != null) {
+                Raaka_aine vanha = (Raaka_aine) findOne(uusi);
+                Connection con = getConnection();
+                PreparedStatement paivita = con.prepareStatement(""
+                        + "UPDATE Raakaine SET (mittayksikko = ?, kuvaus = ? WHERE nimi = ?");
+                paivita.setString(1, uusi.getMittayksikko());
+                paivita.setString(2, uusi.getKuvaus());
+                paivita.setString(3, uusi.getNimi());
+                paivita.executeUpdate();
+                paivita.close();
+                con.close();
+
+            } else {
+
+                Connection con = getConnection();
+                PreparedStatement lisaa = con.prepareStatement("INSERT INTO Raakaaine (nimi, mittayksikko, kuvaus) "
+                        + "VALUES (?, ?, ?)");
+                lisaa.setString(1, uusi.getNimi());
+                lisaa.setString(2, uusi.getMittayksikko());
+                lisaa.setString(3, uusi.getKuvaus());
+
+                lisaa.executeUpdate();
+                lisaa.close();
+                con.close();
+            }
+
         } catch (SQLException e) {
             System.out.println("ongelma lisätessä uutta raaka-ainetta" + e.getMessage());
         }
         return true;
-        
+
     }
 
     @Override
     public void delete(Object key) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
-    
+
 }
