2588a20b156ac74221a1e42962bc0e9ba8b6f5f3
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-02-25 20:01:20 +0200

    resepti.html:n säätöä

--- src/main/java/Webserver/Main.java
+++ src/main/java/Webserver/Main.java
@@ -28,10 +28,9 @@ public class Main {
         RaakaaineDao raakaDao = new RaakaaineDao();
         raakaDao.saveOrUpdate(new Raaka_aine(0, "soijarouhe", "g", "ruskea soijarouhe, toimii siinä missä jauhelihakin"));
         raakaDao.saveOrUpdate(new Raaka_aine(0, "jauheliha", "g", "jauheliha, toimii siinä missä soijarouhekin"));
-        ArrayList<Raaka_aine> jauhelihat = (ArrayList)raakaDao.findByNameCaseIns("jauheliha");
-        
         AnnosRaakaaineDao annosaineDao = new AnnosRaakaaineDao();
         //Integer annosid, Integer raakaaineid, Integer jarjestys, Integer maara, String ohje)
+        ArrayList<Raaka_aine> jauhelihat = (ArrayList)raakaDao.findByNameCaseIns("jauheliha");
         annosaineDao.saveOrUpdate(new AnnosRaakaaine(keitto.getId(), jauhelihat.get(0).getId(), 1, 400, "Paista ja lisää liemeen"));
 
         // Tämä asettaa herokun portin ympäristömuuttujan määräämäksi,
