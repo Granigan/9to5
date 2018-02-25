355463784be5147079e62158b952d05a2b5e2ccc
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-02-25 13:00:38 +0200

    raaka-aineen delete

--- src/main/java/Webserver/Main.java
+++ src/main/java/Webserver/Main.java
@@ -97,10 +97,16 @@ public class Main {
             return " ";
         });
 
-        Spark.post("/raaka_aineet/delete", (req, res) -> {
+        Spark.post("/raaka_aineet/delete/:id", (req, res) -> {
 
-            String nimi = req.queryParams("raaka_aine");
-            
+            String id = req.params(":id");
+            try {
+                Raaka_aine dummy = new Raaka_aine();
+                dummy.setId(Integer.parseInt(id));
+                raakaDao.delete(dummy);
+            } catch (Exception e) {
+                System.out.println("Raaka-aineen delete: Yritettiin muuntaa id integeriksi siinä onnistumatta");
+            }
             /*
              TODO: 
             if (reseptidao.isUsed(nimi)) {
@@ -109,10 +115,10 @@ public class Main {
                 raakaDao.delete(nimi);
             }
             */
-            return "<p>9to5 Corp has not implented this feature as of yet.</p>";
+            //return "<p>9to5 Corp has not implented this feature as of yet.</p>";
             // Lopuksi redirect, kun kaikki ominaisuudet luotu
-           // res.redirect("/raaka_aineet");
-            //return " ";
+            res.redirect("/raaka_aineet");
+            return " ";
         });
 
         Spark.get("/reseptit", (req, res) -> {
